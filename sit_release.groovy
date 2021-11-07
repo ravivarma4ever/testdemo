55cf8c55d25c1b6d7d3439b8cf2f642194c5d4c9
ravivarma4ever <48075919+ravivarma4ever@users.noreply.github.com>
2021-11-07 17:45:24 +0800

    Update sit_release.groovy

--- sit_release.groovy
+++ sit_release.groovy
@@ -7,6 +7,7 @@
           script{
             echo "Build success"   
             Ansible_deploy()
+            nexusArtifactUploader artifacts: [[artifactId: 'demo', classifier: '', file: 'demo.zip', type: 'zip']], credentialsId: 'LOCALID', groupId: 'com.demo', nexusUrl: 'nexus.ravi.com/repository', nexusVersion: 'nexus3', protocol: 'https', repository: 'demo', version: '1'
           }
         }  
       }
