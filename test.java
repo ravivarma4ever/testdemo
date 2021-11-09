a1872ee953f7da55873ead9b11f80aeec0b3b1dc
ravivarma4ever <48075919+ravivarma4ever@users.noreply.github.com>
2021-11-09 21:40:52 +0800

    Update test.java

--- test.java
+++ test.java
@@ -21,3 +21,24 @@ public static void main(String[] args) {
       
 }  
 }  
+package com.java;  
+  
+public class test1 {  
+  
+void msg()  
+ {  
+     System.out.println("Try to access default method outside the package ");  
+ }  
+       
+}  
+//save by DefaultExample2.java  
+package com.javatpoint;  
+import com.java.test1;  
+  
+public class DefaultExample2 {  
+public static void main(String[] args) {  
+    test1 a=new test1();  
+   a.msg();  
+      
+}  
+}  
