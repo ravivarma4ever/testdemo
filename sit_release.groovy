476d3d0bfbcf9aeeaec9d2360ee25bdae250ef87
ravivarma4ever <48075919+ravivarma4ever@users.noreply.github.com>
2021-11-12 22:40:53 +0800

    Update sit_release.groovy

--- sit_release.groovy
+++ sit_release.groovy
@@ -1,18 +1,59 @@
-
-  pipeline{
-    agent { label 'master'}
+pipeline{
+    agent any
     stages{
-      stage('Build'){
-        steps{
-          script{
-            echo "Build success"   
-            Ansible_deploy()
-            nexusArtifactUploader artifacts: [[artifactId: 'demo', classifier: '', file: 'demo.zip', type: 'zip']], credentialsId: 'LOCALID', groupId: 'com.demo', nexusUrl: 'nexus.ravi.com/repository', nexusVersion: 'nexus3', protocol: 'https', repository: 'demo', version: '1'
-          }
-        }  
-      }
+        stage('scmcheckout'){
+            steps{
+                script{
+                    git branch: 'main', credentialsId: 'BITBUCKET', url: 'https://github.com/ravivarma4ever/testdemo.git'
+                }
+            }
+        }
+        stage('scan'){
+            steps{
+                script{
+                    sh"echo 'test' > test1.txt"
+                    withSonarQubeEnv(credentialsId: 'SONAR') {
+                        sh"/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=demo -Dsonar.sources=. -Dsonar.java.binaries=."
+                        echo "scan is success"
+                    }
+                }
+            }
+        }
+        stage('upload'){
+            steps{
+                script{
+                    echo "upload to artifactory"
+                    rtServer (
+    id: 'Artifactory-1',
+    url: 'https://ravivarma.jfrog.io/artifactory',
+    // If you're using username and password:
+    //username: 'user',
+    //password: 'password',
+    credentialsId: 'JFROG',
+    bypassProxy: false,
+    timeout: 300
+)
+                    rtUpload (
+    serverId: 'Artifactory-1',
+    spec: '''{
+          "files": [
+            {
+              "pattern": "test1.txt",
+              "target": "demo-libs-release-local/com/demo/${BUILd_NUMBER}"
+            }
+         ]
+    }'''
+    //buildName: 'demo',
+    //buildNumber: '1',
+    //project: 'demo-libs-release-local'
+)
+                }
+            }
+        }
     }
-  }
-def Ansible_deploy(){
- println"Ansible Deploy" 
+    post{
+        always{
+            cleanWs()
+        }
+    }
 }
